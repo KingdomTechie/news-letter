031e894a7895efe949b6c2ffb4eef2637c096cb5
Keith Powell <keith@tapcart.co>
2021-09-12 13:44:05 -0400

    Added a public folder to serve local assets

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const request = require("request")
 const app = express();
 
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname))
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
 
 app.listen(3000, () => {
     console.log("Newsletter server is lisening on port 3000")
