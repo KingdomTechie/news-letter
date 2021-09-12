bc3a9a9b030e2d72bb2ebab3ff886dfd56c696ba
Keith Powell <keith@tapcart.co>
2021-09-12 16:47:02 -0400

    Refactored error handling

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post("/", function (req, res) {
             }, 
         ]
     }
-
+    // This packages the data payload so that it can be sent to the API 
     let jsonData = JSON.stringify(data)
 
     let listID = "7fca9a448f"
@@ -40,12 +40,20 @@ app.post("/", function (req, res) {
 
     const options = {
         method: "POST",
-        auth: "kpowell:95e3bef06388b5b83b8c64b81bee3aba-us5"
+        auth: "kpowell:90b44a2f743cf508b64ee9421f841c29-us"
     }
 
     const request = https.request(url, options, function (response) {
         response.on("data", function(data) {
-            console.log(JSON.parse(data));
+            
+            let responseStatus = response.statusCode
+
+            if(responseStatus >= 200 && responseStatus <= 299) {
+                res.sendFile(__dirname + "/success.html")
+            } else {
+                res.sendFile(__dirname + "/failure.html")
+            }
+            
         })
     })
 
